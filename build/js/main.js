27835b12452679c13f2bc6684a1782e827237fed
Jet Up <anatolyi.konyk@gmail.com>
2022-02-04 18:02:12 +0200

    Fix main menu, add Trade, add Hospitality Supplies

--- build/js/main.js
+++ build/js/main.js
@@ -1,27 +1,21 @@
 document.querySelector('.sub a').onclick = function () {
     document.querySelector('#left-block').classList.toggle('vis');
     document.querySelector('.sub').classList.toggle('active');
+ 
+    var moo = document.querySelectorAll('.arr');
+    var active = moo[0];
 
-    /*let blocks = document.querySelectorAll( ".subsub" ); // находим все элементы с классом block
-    for( let i = 0; i < blocks.length; i++){ // проходим циклом по всем элементам объекта
-        blocks[i].onclick = function () {
-            this.classList.toggle('active');
-        };
-    }*/
-
-    let dropDowns = Array.from(document.querySelectorAll('.subsub'));
-
-    const handleClick = (e) => {
-        e.preventDefault();
-        dropDowns.forEach(node => {
-            node.classList.remove('active');
-        });
-        e.currentTarget.classList.toggle('active');
-    }
-
-    dropDowns.forEach(node => {
-        node.addEventListener('click', handleClick)
+    for( var i = 0; i < moo.length; i++ ){
+    moo[i].addEventListener('click', function(){
+        if( active == this ){
+            this.closest('li.subsub').classList.toggle('active');
+        } else {
+            active.closest('li.subsub').classList.remove('active');
+            this.closest('li.subsub').classList.add('active');
+            active = this;
+        }
     });
+    }
 
 };
 
@@ -29,10 +23,33 @@ document.querySelector('.header-search svg').onclick = function () {
     document.querySelector('input').classList.toggle('active');
     document.querySelector('.header-search').classList.toggle('visible');
 }
-
 document.querySelector('.right-part__block.pr .link').onclick = function () {
     document.querySelector('.right-part__block.pr').classList.toggle('active');
 }
 document.querySelector('.right-part__block.lang .link').onclick = function () {
     document.querySelector('.right-part__block.lang').classList.toggle('active');
 }
+
+// Scroll to top
+const showOnPx = 100;
+const backToTopButton = document.querySelector(".back-to-top");
+
+const scrollContainer = () => {
+  return document.documentElement || document.body;
+};
+
+const goToTop = () => {
+  document.body.scrollIntoView({
+    behavior: "smooth"
+  });
+};
+
+document.addEventListener("scroll", () => {
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.remove("hidden");
+  } else {
+    backToTopButton.classList.add("hidden");
+  }
+});
+
+backToTopButton.addEventListener("click", goToTop);
